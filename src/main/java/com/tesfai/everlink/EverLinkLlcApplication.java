d4d4f73e8961027015a3551ea6b091c1ba11155d
tesfai05 <tesfai275@gmail.com>
2025-07-09 21:37:52 -0500

    Fix app crash

--- src/main/java/com/tesfai/everlink/EverLinkLlcApplication.java
+++ src/main/java/com/tesfai/everlink/EverLinkLlcApplication.java
@@ -2,12 +2,20 @@ package com.tesfai.everlink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class EverLinkLlcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EverLinkLlcApplication.class, args);
+		SpringApplication app = new SpringApplication(EverLinkLlcApplication.class);
+		// Let Heroku set the port
+		String port = System.getenv("PORT");
+		if (port != null) {
+			app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		}
+		app.run(args);
 	}
-
 }
