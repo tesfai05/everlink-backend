505d77fa9e8253458234f36dd7e320a0b5ff1400
tesfai05 <tesfai275@gmail.com>
2025-07-21 14:13:28 -0500

    Add admin on start up.

---
+++ src/main/java/com/tesfai/everlink/config/StartupAdminLoader.java
@@ -0,0 +1,43 @@
+package com.tesfai.everlink.config;
+
+import com.tesfai.everlink.entity.Role;
+import com.tesfai.everlink.entity.User;
+import com.tesfai.everlink.repository.IRoleRepository;
+import com.tesfai.everlink.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class StartupAdminLoader {
+    @Value(value = "${ADMIN_USERNAME}")
+    private String adminUsername;
+    @Value(value = "${ADMIN_PASSWORD}")
+    private String adminPassword;
+
+    @Bean
+    public CommandLineRunner loadDefaultAdmin(IUserRepository userRepository, PasswordEncoder passwordEncoder, IRoleRepository roleRepository) {
+        return args -> {
+            boolean adminExists = userRepository
+                    .findAll()
+                    .stream()
+                    .map(user -> user.getRoles())
+                    .flatMap(u->u.stream().map(us->us.getName()))
+                    .anyMatch(user -> user.equalsIgnoreCase("ADMIN"));
+
+            if (!adminExists) {
+                User admin = new User();
+                admin.setUsername(adminUsername);
+                admin.setPassword(passwordEncoder.encode(adminPassword));
+                Role userRole = roleRepository.findByName("ADMIN")
+                        .orElseThrow(() -> new RuntimeException("ADMIN role not found"));
+                admin.setRoles(Set.of(userRole));
+                userRepository.save(admin);
+            }
+        };
+    }
+}
